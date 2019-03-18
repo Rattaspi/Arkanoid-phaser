bd5ffd2288de64c7e1e50567a4d01ee8e950084a
Alex Canut <a.canutperdigon@gmail.com>
2019-03-18 18:22:31 +0100

    Adjusted avatar position

--- src/arkanoidScene.js
+++ src/arkanoidScene.js
@@ -7,8 +7,8 @@ arkanoid.mainScene = {
 	},
 
 	create: function () {
-		this.avatar = this.add.image(sceneConfig.width/2, sceneConfig.height/2, "black");
-		this.avatar.setOrigin(1.0,0.5);
+		this.avatar = this.add.image(sceneConfig.width/2, sceneConfig.height * 0.95, "black");
+		this.avatar.setOrigin(.5);
 		//console.log(this.avatar);
 		this.avatar.scaleX = 60;
 		this.avatar.scaleY = 7;
@@ -16,6 +16,7 @@ arkanoid.mainScene = {
 
 	update: function () {
 		
-		this.avatar.x = this.game.input.mousePointer.x;
+		this.avatar.x = this.game.input.mousePointer.x + (this.avatar.displayWidth/2);
+		
 	}
 };
